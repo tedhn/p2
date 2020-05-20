939788373d94171cbd7aa5ad511d996fb81fc181
ted <heinhtetnaing186@gmail.com>
2020-05-20 17:31:14 +0630

    Updates

---
+++ precache-manifest.1bc8457a3a8abd61e5778f2babdb47e6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e45c89292ca70f33c67a8ef114aab55",
+    "url": "/p2/index.html"
+  },
+  {
+    "revision": "ea9b6adb43fa3394128d",
+    "url": "/p2/static/css/main.ac33b586.chunk.css"
+  },
+  {
+    "revision": "76b4380e2f8a544c6fbf",
+    "url": "/p2/static/js/2.93f51b65.chunk.js"
+  },
+  {
+    "revision": "c344ff0e273d3dd0833ad3a0d4c0857e",
+    "url": "/p2/static/js/2.93f51b65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea9b6adb43fa3394128d",
+    "url": "/p2/static/js/main.152df33c.chunk.js"
+  },
+  {
+    "revision": "ae8923008db6a0fd7ce5",
+    "url": "/p2/static/js/runtime-main.8df2dfe1.js"
+  },
+  {
+    "revision": "b82da370bcc0339b12829df5a9291db1",
+    "url": "/p2/static/media/amino.b82da370.png"
+  },
+  {
+    "revision": "6546cae3099605a76a8cf4555884053e",
+    "url": "/p2/static/media/email.6546cae3.svg"
+  },
+  {
+    "revision": "544a5ba185aa7cc4b3122ccc8efd22af",
+    "url": "/p2/static/media/fb.544a5ba1.svg"
+  },
+  {
+    "revision": "b6d060285eab9088091ec53592757cae",
+    "url": "/p2/static/media/gh.b6d06028.svg"
+  },
+  {
+    "revision": "66ad6d455ba3683123375e3f48180586",
+    "url": "/p2/static/media/ig.66ad6d45.svg"
+  },
+  {
+    "revision": "4a2fd77166648d0262ac7d21d10a7419",
+    "url": "/p2/static/media/lindathai.4a2fd771.png"
+  },
+  {
+    "revision": "19f180e188c00602750def1c2d4832d3",
+    "url": "/p2/static/media/portfoliov1.19f180e1.png"
+  },
+  {
+    "revision": "74b4a68bd22e0ad8fd45d3e2ad4686fd",
+    "url": "/p2/static/media/windia.74b4a68b.png"
+  }
+]);
